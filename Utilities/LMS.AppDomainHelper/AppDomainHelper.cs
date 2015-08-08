97a039352b77f8d0086ee4f5381d2d1bfb47f9bb
Lennart <lms@lms-dev.com>
2015-08-09 00:54:48 +0200

    Added Vector3Extension Added LMS.AppDomainHelper project

---
+++ Utilities/LMS.AppDomainHelper/AppDomainHelper.cs
@@ -0,0 +1,88 @@
+﻿namespace LMS.AppDomainHelper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    using mscoree;
+
+    public class AppDomainHelper
+    {
+        public delegate void CrossAppDomainCallDelegate(params object[] payload);
+        public delegate object CrossAppDomainCallRetValueDelegate(params object[] payload);
+
+        public static AppDomain GetAppDomainByName(string name)
+        {
+            return GetAppDomains().FirstOrDefault(appDomain => appDomain.FriendlyName == name);
+        }
+
+        public static void InvokeOnAppDomain(AppDomain appDomain, CrossAppDomainCallDelegate targetFunc, params object[] payload)
+        {
+            appDomain.SetData(appDomain.FriendlyName + "_payload", payload);
+            appDomain.SetData(appDomain.FriendlyName + "_func", targetFunc);
+            appDomain.DoCallBack(InvokedOnAppDomain);
+        }
+
+        public static T InvokeOnAppDomain<T>(AppDomain appDomain, CrossAppDomainCallRetValueDelegate targetFunc, params object[] payload)
+        {
+            appDomain.SetData(appDomain.FriendlyName + "_payload", payload);
+            appDomain.SetData(appDomain.FriendlyName + "_func", targetFunc);
+            appDomain.DoCallBack(InvokedOnAppDomainRet);
+
+            return (T)Convert.ChangeType(appDomain.GetData("result"), typeof(T));
+        }
+
+        private static void InvokedOnAppDomain()
+        {
+            AppDomain currentAppDomain = AppDomain.CurrentDomain;
+            string name = currentAppDomain.FriendlyName;
+
+            // Grab payload.
+            object[] payload = (object[])currentAppDomain.GetData(name + "_payload");
+            CrossAppDomainCallDelegate func = (CrossAppDomainCallDelegate)currentAppDomain.GetData(name + "_func");
+
+            func.Invoke(payload);
+        }
+
+        private static void InvokedOnAppDomainRet()
+        {
+            AppDomain currentAppDomain = AppDomain.CurrentDomain;
+            string name = currentAppDomain.FriendlyName;
+
+            // Grab payload.
+            object[] payload = (object[])currentAppDomain.GetData(name + "_payload");
+            CrossAppDomainCallRetValueDelegate func = (CrossAppDomainCallRetValueDelegate)currentAppDomain.GetData(name + "_func");
+
+            object result = func.Invoke(payload);
+            currentAppDomain.SetData("result", result);
+        }
+
+        public static IList<AppDomain> GetAppDomains()
+        {
+            IList<AppDomain> domains = new List<AppDomain>();
+            IntPtr enumHandle = IntPtr.Zero;
+            ICorRuntimeHost host = new CorRuntimeHost();
+
+            try
+            {
+                host.EnumDomains(out enumHandle);
+                while (true)
+                {
+                    object domain;
+                    host.NextDomain(enumHandle, out domain);
+                    if (domain == null) break;
+                    AppDomain appDomain = (AppDomain)domain;
+                    domains.Add(appDomain);
+                }
+
+                return domains;
+            }
+            finally
+            {
+                host.CloseEnum(enumHandle);
+                Marshal.ReleaseComObject(host);
+            }
+        } 
+    }
+}
---
+++ Utilities/LMS.AppDomainHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("LMS.AppDomainHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LMS.AppDomainHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f3645d74-82df-4273-8a45-cd430e474d72")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
