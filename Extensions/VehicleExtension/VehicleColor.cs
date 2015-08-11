bc0ccbf2517b03b4100e269c5c6d58139a412ede
alexguirre <alexguirre.aa@gmail.com>
2015-08-11 07:05:32 +0200

    Create VehicleColor.cs
    Credit to Stealth22 for this struct

---
+++ Extensions/VehicleExtension/VehicleColor.cs
@@ -0,0 +1,54 @@
+namespace alexguirre.Common.Extensions
+{
+    using Rage;
+    using Rage.Native;
+    using alexguirre.Common.Enum;
+    using System.Drawing;
+    using System;
+
+    /// <summary>
+    /// Struct for vehicles primary and secondary colors
+    /// </summary>
+    public struct VehicleColor
+    {
+        /// <summary>
+        /// The primary color paint index 
+        /// </summary>
+        public EPaint PrimaryColor { get; set; }
+
+        /// <summary>
+        /// The secondary color paint index 
+        /// </summary>
+        public EPaint SecondaryColor { get; set; }
+
+
+
+        /// <summary>
+        /// Gets the primary color name
+        /// </summary>
+        public string PrimaryColorName
+        {
+            get { return GetColorName(PrimaryColor); }
+        }
+        /// <summary>
+        /// Gets the secondary color name
+        /// </summary>
+        public string SecondaryColorName
+        {
+            get { return GetColorName(SecondaryColor); }
+        }
+
+
+
+        /// <summary>
+        /// Gets the color name
+        /// </summary>
+        /// <param name="paint">Color to get the name from</param>
+        /// <returns></returns>
+        public string GetColorName(EPaint paint)
+        {
+            String name = Enum.GetName(typeof(EPaint), paint);
+            return name.Replace("_", " ");
+        }
+    }
+}
