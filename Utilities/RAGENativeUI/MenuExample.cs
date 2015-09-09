9b787e3091a0375d5ac2d7ff676097ca121bd6a0
alexguirre <alexguirre.aa@gmail.com>
2015-09-09 22:08:26 +0200

    Update MenuExample.cs

--- Utilities/RAGENativeUI/MenuExample.cs
+++ Utilities/RAGENativeUI/MenuExample.cs
@@ -1,32 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
-using GTA;
-using GTA.Native;
-using NativeUI;
+[assembly: Rage.Attributes.Plugin("MenuExample", Author = "alexguirre", Description = "Example using RAGENativeUI")]
 
-public class MenuExample : Script
+namespace MenuExample
 {
-    private UIMenu mainMenu;
-    private UIMenu newMenu;
-    private UIMenuCheckboxItem ketchupCheckbox;
-    private UIMenuListItem dishesListItem;
-    private UIMenuItem cookItem;
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Rage;
+    using Rage.Native;
+    using RAGENativeUI;
+    using RAGENativeUI.Elements;
 
-    private MenuPool _menuPool;
-
-    public MenuExample()
+    public static class EntryPoint
     {
-        Tick += OnTick;
-        KeyDown += OnKeyDown;
-        _menuPool = new MenuPool();
+        private static UIMenu mainMenu;
+        private static UIMenu newMenu;
+        private static MenuCheckboxItem ketchupCheckbox;
+        private static MenuListItem dishesListItem;
+        private static NativeMenuItem cookItem;
 
-        mainMenu = new UIMenu("Native UI", "~b~NATIVEUI SHOWCASE");
-        _menuPool.Add(mainMenu);
+        private static NativeMenuItem spawnCar;
+        private static MenuListItem carsList;
 
-        mainMenu.AddItem(ketchupCheckbox = new UIMenuCheckboxItem("Add ketchup?", false, "Do you wish to add ketchup?"));
-        var foods = new List<dynamic>
+        private static MenuPool _menuPool;
+
+        public static void Main()
+        {
+            Game.FrameRender += Process;
+            _menuPool = new MenuPool();
+
+            mainMenu = new UIMenu("RAGENative UI", "~b~RAGENATIVEUI SHOWCASE");
+
+            _menuPool.Add(mainMenu);
+
+            mainMenu.AddItem(ketchupCheckbox = new MenuCheckboxItem("Add ketchup?", false, "Do you wish to add ketchup?"));
+            var foods = new List<dynamic>
         {
             "Banana",
             "Apple",
@@ -34,72 +42,104 @@ public class MenuExample : Script
             "Quartilicious",
             0xF00D, // Dynamic!
         };
-        mainMenu.AddItem(dishesListItem = new UIMenuListItem("Food", foods, 0));
-        mainMenu.AddItem(cookItem = new UIMenuItem("Cook!", "Cook the dish with the appropiate ingredients and ketchup."));
+            mainMenu.AddItem(dishesListItem = new MenuListItem("Food", foods, 0));
+            mainMenu.AddItem(cookItem = new NativeMenuItem("Cook!", "Cook the dish with the appropiate ingredients and ketchup."));
 
-        var menuItem = new UIMenuItem("Go to another menu.");
-        mainMenu.AddItem(menuItem);
-        cookItem.SetLeftBadge(UIMenuItem.BadgeStyle.Star);
-        cookItem.SetRightBadge(UIMenuItem.BadgeStyle.Tick);
-        mainMenu.RefreshIndex();
+            var menuItem = new NativeMenuItem("Go to another menu.");
+            mainMenu.AddItem(menuItem);
+            cookItem.SetLeftBadge(NativeMenuItem.BadgeStyle.Star);
+            cookItem.SetRightBadge(NativeMenuItem.BadgeStyle.Tick);
 
-        mainMenu.OnItemSelect += OnItemSelect;
-        mainMenu.OnListChange += OnListChange;
-        mainMenu.OnCheckboxChange += OnCheckboxChange;
-        mainMenu.OnIndexChange += OnItemChange;
+            var carsModel = new List<dynamic>
+            {
+                "Adder",
+                "Bullet",
+                "Police",
+                "Police2",
+                "Asea",
+                "FBI",
+                "FBI2",
+                "Firetruk",
+                "Ambulance",
+                "Rhino",
+            };
+            carsList = new MenuListItem("Cars Models", carsModel, 0);
+            mainMenu.AddItem(carsList);
 
-        newMenu = new UIMenu("Native UI", "~r~NATIVEUI SHOWCASE");
-        _menuPool.Add(newMenu);
-        for (int i = 0; i < 20; i++)
-        {
-            newMenu.AddItem(new UIMenuItem("PageFiller", "Sample description that takes more than one line. Moreso, it takes way more than two lines since it's so long. Wow, check out this length!"));
+            spawnCar = new NativeMenuItem("Spawn Car");
+            mainMenu.AddItem(spawnCar);
+
+            mainMenu.RefreshIndex();
+
+            mainMenu.OnItemSelect += OnItemSelect;
+            mainMenu.OnListChange += OnListChange;
+            mainMenu.OnCheckboxChange += OnCheckboxChange;
+            mainMenu.OnIndexChange += OnItemChange;
+            
+
+            newMenu = new UIMenu("RAGENative UI", "~b~RAGENATIVEUI SHOWCASE");
+            _menuPool.Add(newMenu);
+            for (int i = 0; i < 35; i++)
+            {
+                newMenu.AddItem(new NativeMenuItem("PageFiller " + i.ToString(), "Sample description that takes more than one line. Moreso, it takes way more than two lines since it's so long. Wow, check out this length!"));
+            }
+            newMenu.RefreshIndex();
+            mainMenu.BindMenuToItem(newMenu, menuItem);
+
+            while (true)
+                GameFiber.Yield();
         }
-        newMenu.RefreshIndex();
-        mainMenu.BindMenuToItem(newMenu, menuItem);
-        
-    }
 
-    public void OnItemChange(UIMenu sender, int index)
-    {
-        sender.MenuItems[index].SetLeftBadge(UIMenuItem.BadgeStyle.None);
-    }
 
-    public void OnCheckboxChange(UIMenu sender, UIMenuCheckboxItem checkbox, bool Checked)
-    {
-        if (sender != mainMenu || checkbox != ketchupCheckbox) return; // We only want to detect changes from our menu.
-        UI.Notify("~r~Ketchup status: ~b~" + Checked);
-    }
-
-    public void OnListChange(UIMenu sender, UIMenuListItem list, int index)
-    {
-        if (sender != mainMenu || list != dishesListItem) return; // We only want to detect changes from our menu.
-        string dish = list.IndexToItem(index).ToString();
-        UI.Notify("Preparing ~b~" + dish +"~w~...");
-    }
-    
-    public void OnItemSelect(UIMenu sender, UIMenuItem selectedItem, int index)
-    {
-        if (sender != mainMenu || selectedItem != cookItem) return; // We only want to detect changes from our menu and our button.
-        // You can also detect the button by using index
-        string dish = dishesListItem.IndexToItem(dishesListItem.Index).ToString();
-        bool ketchup = ketchupCheckbox.Checked;
-
-        string output = ketchup
-            ? "You have ordered ~b~{0}~w~ ~r~with~w~ ketchup."
-            : "You have ordered ~b~{0}~w~ ~r~without~w~ ketchup.";
-        UI.ShowSubtitle(String.Format(output, dish));
-    }
-
-    public void OnTick(object o, EventArgs e)
-    {
-        _menuPool.ProcessMenus();
-    }
-
-    public void OnKeyDown(object o, KeyEventArgs e)
-    {
-        if (e.KeyCode == Keys.F5 && !_menuPool.IsAnyMenuOpen()) // Our menu on/off switch
+        public static void OnItemChange(UIMenu sender, int index)
         {
-            mainMenu.Visible = !mainMenu.Visible;
+            sender.MenuItems[index].SetLeftBadge(NativeMenuItem.BadgeStyle.None);
+        }
+
+        public static void OnCheckboxChange(UIMenu sender, MenuCheckboxItem checkbox, bool Checked)
+        {
+            if (sender != mainMenu || checkbox != ketchupCheckbox) return; // We only want to detect changes from our menu.
+            Game.DisplayNotification("~r~Ketchup status: ~b~" + Checked);
+        }
+
+        public static void OnListChange(UIMenu sender, MenuListItem list, int index)
+        {
+            if (sender != mainMenu || list != dishesListItem) return; // We only want to detect changes from our menu.
+            string dish = list.IndexToItem(index).ToString();
+            Game.DisplayNotification("Preparing ~b~" + dish + "~w~...");
+        }
+
+        public static void OnItemSelect(UIMenu sender, NativeMenuItem selectedItem, int index)
+        {
+            if (sender != mainMenu) return; // We only want to detect changes from our menu and our button.
+            // You can also detect the button by using index
+
+            if (selectedItem == cookItem)
+            {
+                string dish = dishesListItem.IndexToItem(dishesListItem.Index).ToString();
+                bool ketchup = ketchupCheckbox.Checked;
+
+                string output = ketchup
+                    ? "You have ordered ~b~{0}~w~ ~r~with~w~ ketchup."
+                    : "You have ordered ~b~{0}~w~ ~r~without~w~ ketchup.";
+                Game.DisplaySubtitle(String.Format(output, dish), 5000);
+            }
+            else if (selectedItem == spawnCar)
+            {
+                GameFiber.StartNew(delegate
+                {
+                    Vehicle veh = new Vehicle(((string)carsList.IndexToItem(carsList.Index)).ToLower(), Game.LocalPlayer.Character.Position + Game.LocalPlayer.Character.ForwardVector * 5.6f);
+                    veh.Dismiss();
+                }); 
+            }
+        }
+
+        public static void Process(object sender, GraphicsEventArgs e)
+        {
+            if (Game.IsKeyDown(Keys.F5) && !_menuPool.IsAnyMenuOpen()) // Our menu on/off switch
+                mainMenu.Visible = !mainMenu.Visible;
+
+            _menuPool.ProcessMenus();
         }
     }
 }
